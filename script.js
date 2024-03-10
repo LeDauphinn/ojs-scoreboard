2f1b9a76bf95647dae7719eb0079737ae92bdcb3
Yunus Eren Türkeri <82608558+LeDauphinn@users.noreply.github.com>
2024-03-10 18:00:35 +0300

    scripts remaining

---
+++ script.js
@@ -0,0 +1,12 @@
+const teamA_score = document.getElementById('teamA_score');
+const teamB_score = document.getElementById('teamB_score');
+
+const decrementA = document.getElementById('decrementA');
+const incrementA = document.getElementById('incrementA');
+const edit_nameA = document.getElementById('edit_nameA');
+const edit_scoreA = document.getElementById('edit_scoreA');
+
+const decrementB = document.getElementById('decrementB');
+const incrementB = document.getElementById('incrementB');
+const edit_nameB = document.getElementById('edit_nameB');  
+const edit_scoreB = document.getElementById('edit_scoreB');
